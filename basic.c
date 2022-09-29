bfff2ad2cce2b5b84fe8eee2eb8c1b071b6bdacc
AkshatPandey-2004 <114497044+AkshatPandey-2004@users.noreply.github.com>
2022-09-29 12:46:28 +0530

    created basic c file

---
+++ basic.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("************MENU ************");
+    printf("PRESS FOLLOWING TO DEMONSTRATE FOLLOWING CONDITIONS");
+    printf("Enter First Number: ");
+    scanf("%d",&)
+    printf("\n 1: For loop");
+    printf("\n 2: While loop");
+    printf("\n 3: Do while loop");
+    printf("\n 4: If else");
+    printf("\n 5: Exit ");
+    printf("\nEnter: ");
+    scanf("%d",&n);
+    if(n==1){
+        
+    }
+    else if(n==2){
+        printf("2");
+    }
+    else if(n==3){
+        printf("3");
+    }
+    else if(n==4){
+        printf("4");
+    }
+    else if(n==5){
+        printf("5"); 
+    }
+
+}
